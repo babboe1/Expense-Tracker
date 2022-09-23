918f3251a66019d43b82a9eb71126191742b693e
babboeCodes <babboecodes@gmail.com>
2022-09-23 14:22:34 +0100

    create Component - add expenseItem component - add expensedate component

--- src/Components/ExpenseItem/ExpenseItem.js
+++ src/Components/ExpenseItem/ExpenseItem.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate/ExpenseDate';
+import './ExpenseItem.css';
 
-const ExpenseItem = () => {
-   
+const ExpenseItem = (props) => {
+
    return (
       <div className="expense-item">
-         <div>March 2022</div>
+         <ExpenseDate date={ props.date} />
          <div className="expense-item__description">
-            <h2>Car Insurance</h2>
-            <div className="expense-item__price">$294.67</div>
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">${props.amount}</div>
          </div>
       </div>
    );
