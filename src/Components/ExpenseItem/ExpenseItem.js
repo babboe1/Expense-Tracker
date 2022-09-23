63431b6aa4c041edb949a7a1cfe097b2e31264a2
babboeCodes <babboecodes@gmail.com>
2022-09-23 13:06:33 +0100

    initial commit

---
+++ src/Components/ExpenseItem/ExpenseItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ExpenseItem.css'
+
+const ExpenseItem = () => {
+   
+   return (
+      <div className="expense-item">
+         <div>March 2022</div>
+         <div className="expense-item__description">
+            <h2>Car Insurance</h2>
+            <div className="expense-item__price">$294.67</div>
+         </div>
+      </div>
+   );
+};
+
+export default ExpenseItem;
