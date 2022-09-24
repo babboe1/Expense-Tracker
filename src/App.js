b7f43257f5e61e2290f764f7b78b86f3dfea16a7
babboeCodes <babboecodes@gmail.com>
2022-09-24 19:40:02 +0100

    update app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import ExpenseItem from './Components/ExpenseItem/ExpenseItem';
+import React, { useState } from 'react';
+import Card from './Components/Card/Card';
+import Context from './Components/Context/Context';
+import ExpenseItems from './Components/ExpenseItems/ExpenseItems';
+import NewExpense from './Components/NewExpense/NewExpense';
 
 const App = () => {
+   const [expense, setExpenseData] = useState("");
+   // console.log(expense);
+   
+   const getDataHandler = (newData) => {
+      setExpenseData(prev => {
+         console.log(expense, prev, newData);
+         return [...prev, newData];
+      });
+      
+   };
+
    return (
       <div>
-         <h1>Function React</h1>
-         <ExpenseItem />
+         <Context.Provider
+            value={{
+               newExpenseData: expense,
+            }}
+         >
+            <h1>Function React</h1>
+            <Card>
+               <NewExpense data={(data) => getDataHandler(data)} />
+               <ExpenseItems />
+            </Card>
+         </Context.Provider>
       </div>
    );
 };
