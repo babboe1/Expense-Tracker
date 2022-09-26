6dec8d4144bf3ef2a75ab430a45399e2b50fcb33
babboeCodes <babboecodes@gmail.com>
2022-09-26 08:30:23 +0100

    Create useState function

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from './Components/Card/Card';
 import Context from './Components/Context/Context';
 import ExpenseItems from './Components/ExpenseItems/ExpenseItems';
 import NewExpense from './Components/NewExpense/NewExpense';
 
 const App = () => {
-   // const [expense, setExpenseData] = useState("");
-   // // console.log(expense);
-   
-   // const getDataHandler = (newData) => {
-   //    setExpenseData(prev => {
-   //       console.log(expense, prev, newData);
-   //       return [...prev, newData];
-   //    });
-      
-   // };
+   const [expense, setExpenseData] = useState([]);
+
+   const getDataHandler = (newData) => {
+      setExpenseData([newData])
+   };
 
    return (
       <div>
          <Context.Provider
             value={{
-               newExpenseData: [],
-               update: () => {},
+               expense,
+               getDataHandler,
             }}
          >
             <h1>Function React</h1>
             <Card>
-               {/* <NewExpense data={(data) => getDataHandler(data)} /> */}
                <NewExpense />
                <ExpenseItems />
             </Card>
